ec1aea9a99a82bff276c20668364d882b487f1d0
Tom Hofkens <Tom.Hofkens@sji.be>
2015-12-06 15:22:30 +0000

    klas kan alle lln van meerdere klassen teruggeven

--- src/Klas.php
+++ src/Klas.php
@@ -1,12 +1,42 @@
 <?php namespace Hofkens\CoreLib;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Klas extends Model
 {
     protected $table = 'klas';
 
+    /**
+     * Geef alle lln van deze klas terug
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function leerlingen(){
         return $this->hasMany('Hofkens\CoreLib\Leerling');
+    }
+
+    /**
+     * ? hoort dat in dit model thuis ?
+     * @param array $klassen
+     * @return array
+     */
+    public static function getLeerlingenUitKlassen(array $klassen){
+        //querybuilder
+        return DB::table('leerling')
+                    ->join('klas','leerling.klas_id','=','klas.id')
+                    ->whereIn('klas.code',$klassen)
+                    ->select('leerling.*')
+                    ->get();
+
+        //ruwe query
+        $klassenstr = implode("','",$klassen);
+        $query = <<<SQL
+          SELECT l.*
+          FROM leerling as l
+          JOIN klas as k
+          ON l.klas_id = k.id
+          WHERE k.code IN ('$klassenstr')
+SQL;
+        return DB::select($query);
     }
 }
